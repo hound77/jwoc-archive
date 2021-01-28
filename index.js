295fc6e31dee39e8a1d7a988071a0e31f34d19c1
Nikhita <vnikhita28@gmail.com>
2021-01-29 02:12:52 +0530

    Added Slider

--- index.js
+++ index.js
@@ -39,3 +39,33 @@ function collegecount(){
     clearInterval(college)
   }
 }
+
+let slideIndex=1;
+showSlides(slideIndex);
+function nextSlide() {
+    showSlides(slideIndex += 1);
+}
+function prevSlide() {
+    showSlides(slideIndex -= 1);  
+}
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+let i;
+let slides = document.getElementsByClassName("carousel-item");
+    
+
+if (n > slides.length) {
+  slideIndex = 1
+  }
+if (n < 1) {
+  slideIndex = slides.length
+ }
+  
+for (let slide of slides) {
+   slide.style.display = "none";
+ }
+slides[slideIndex - 1].style.display = "block";    
+}
